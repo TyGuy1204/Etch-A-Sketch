6a2eaace4609535dd4bbfe6c3d6f60d87e92149e
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-18 12:23:44 -0500

    Add event listener for mouseover and mouseleave to change background of squares

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
 const div_container  = document.querySelector(".div_container");
 for(let i = 0; i < 256; i++){
     const grid_square  = document.createElement("div");
+    grid_square.classList.add("grid_square");
     div_container.appendChild(grid_square);
 }
+const squares = document.querySelectorAll(".grid_square");
+squares.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.cssText = "background-color: blue";
+    });
+    div.addEventListener("mouseleave", () => {
+        div.style.cssText = "background-color:";
+    });
+});
