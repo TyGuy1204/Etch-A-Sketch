c361cfcaa4828810a70b281899ac003e1a9b60db
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-18 10:36:08 -0500

    Create loop to add grid squares to container

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
 const div_container  = document.querySelector(".div_container");
+for(let i = 0; i < 9; i++){
+    const grid_square  = document.createElement("div");
+    div_container.appendChild(grid_square);
+}
