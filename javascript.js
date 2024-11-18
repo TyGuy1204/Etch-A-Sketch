42fbc38c219786d9d45ecc7a0e0004d1324f5ad5
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-18 12:46:41 -0500

    Add prompt to ask for new size on button click

--- javascript.js
+++ javascript.js
@@ -18,7 +18,15 @@ reset_button.addEventListener("click", () =>{
 });
 const resize_button = document.querySelector(".resize_button");
 resize_button.addEventListener("click", () => {
+    let size  = prompt("What size would you like the grid to be (size x size)? :");
     squares.forEach((div) => {
         div.remove();
-    })
+    });
+    /*
+    for(let i = 0; i < (size * size); i++){
+        const grid_square  = document.createElement("div");
+        grid_square.classList.add("grid_square");
+        div_container.appendChild(grid_square);
+    }
+        */
 });
