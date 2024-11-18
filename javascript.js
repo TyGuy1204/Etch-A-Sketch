9e313ab1483fa1388fd815b727160e434023f240
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-18 16:15:24 -0500

    Add flex style to javascript to account for change in grid size and gap size

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const STARTING_SIZE = 16;
+const gap_size = 1;
 const div_container  = document.querySelector(".div_container");
 const reset_button = document.querySelector(".reset_button");
 const resize_button = document.querySelector(".resize_button");
@@ -8,25 +9,27 @@ function draw_grid(grid_size){
     for(let i = 0; i < (grid_size * grid_size); i++){
         const grid_square  = document.createElement("div");
         grid_square.classList.add("grid_square");
+        grid_square.style.flex = `0 0 calc((100% - ${(grid_size - 1) * gap_size}px) / ${grid_size})`;
+        grid_square.style.aspectRatio = '1';
         div_container.appendChild(grid_square);
     }
     squares = document.querySelectorAll(".grid_square");
 }
 function color_grid(){
-    squares.forEach((div) => {
-        div.addEventListener("mouseover", () => {
-            div.style.cssText = "background-color: blue";
+    squares.forEach((grid_square) => {
+        grid_square.addEventListener("mouseover", () => {
+            grid_square.style.backgroundColor = "blue";
         });
     });
 }
 function reset_grid(){
-    squares.forEach((div) => {
-        div.style.cssText = "background-color:";
+    squares.forEach((grid_square) => {
+        grid_square.style.backgroundColor = "";
     })
 }
 function clearGrid(){
-    squares.forEach((div) => {
-        div.remove();
+    squares.forEach((grid_square) => {
+        grid_square.remove();
     });
 }
 
@@ -40,7 +43,7 @@ reset_button.addEventListener("click", () =>{
 
 
 resize_button.addEventListener("click", () => {
-    let size  = prompt("What size would you like the grid to be (size x size)? :");
+    let size  = parseInt(prompt("What size would you like the grid to be (size x size)? :"));
     clearGrid();
     draw_grid(size);
     color_grid();
